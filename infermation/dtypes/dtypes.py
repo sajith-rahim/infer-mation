a13dfec0da179c35e8d2dda725d7dec43343945c
sajith.rahim <sajith.rahim@outlook.com>
2022-02-23 21:18:10 +0530

    Create dtypes.py

---
+++ infermation/dtypes/dtypes.py
@@ -0,0 +1,13 @@
+import enum
+
+
+class dtypes(enum.Enum):
+
+    integer = "integer"
+    float = "float"
+
+    date = "date"
+    datetime = "datetime"
+
+    empty = "empty"
+    invalid = "invalid"
